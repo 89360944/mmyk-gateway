c82029f3217f5f64308ac994e48e18c9b8ef09ba
89360944@qq.com <89360944@qq.com>
2022-04-12 14:56:37 +0800

    test

--- src/main/java/com/example/mmykgateway/MmykGatewayApplication.java
+++ src/main/java/com/example/mmykgateway/MmykGatewayApplication.java
@@ -20,18 +20,19 @@ public class MmykGatewayApplication {
         SpringApplication.run(MmykGatewayApplication.class, args);
     }
 
-    @Autowired
-    RouteDefinitionLocator locator;
+//    @Autowired
+//    RouteDefinitionLocator locator;
+//
+//    @Bean
+//    public List<GroupedOpenApi> apis() {
+//        List<GroupedOpenApi> groups = new ArrayList<>();
+//        List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+//        assert definitions != null;
+//        definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
+//            String name = routeDefinition.getId().replaceAll("-service", "");
+//            groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build());
+//        });
+//        return groups;
+//    }
 
-    @Bean
-    public List<GroupedOpenApi> apis() {
-        List<GroupedOpenApi> groups = new ArrayList<>();
-        List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
-        assert definitions != null;
-        definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
-            String name = routeDefinition.getId().replaceAll("-service", "");
-            groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build());
-        });
-        return groups;
-    }
 }
