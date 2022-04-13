ddf91893e1ad1a422ea50206865484f73308f136
89360944@qq.com <89360944@qq.com>
2022-04-13 13:55:08 +0800

    test

---
+++ src/main/java/com/example/mmykgateway/SwaggerConfig.java
@@ -0,0 +1,91 @@
+package com.example.mmykgateway;
+
+import org.springdoc.core.*;
+import org.springdoc.core.providers.ActuatorProvider;
+import org.springdoc.webflux.api.MultipleOpenApiWebFluxResource;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.result.method.RequestMappingInfoHandlerMapping;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Configuration
+public class SwaggerConfig {
+    @Autowired
+    RouteDefinitionLocator locator;
+    @Autowired
+    DiscoveryClient discoveryClient;
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+    // 手动根据路由定义的服务，创建分组API文档
+    @Bean
+    public List<GroupedOpenApi> apis() {
+        List<GroupedOpenApi> groups = new ArrayList<>();
+        List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+        definitions.stream().forEach(routeDefinition -> {
+            String name = routeDefinition.getUri().getHost();
+            GroupedOpenApi groupedOpenApi =
+                    GroupedOpenApi.builder()
+                            .group(name)
+                            .pathsToMatch(
+                                    routeDefinition.getPredicates().get(0).getArgs().get("pattern")
+                            ).build();
+            groups.add(groupedOpenApi);
+        });
+        return groups;
+    }
+    // 重载默认的多重API定义接口，如果请求的分组为注册的服务ID，则返回对应服务的API定义
+    @Bean
+    MultipleOpenApiWebFluxResource multipleOpenApiResource(List<GroupedOpenApi> groupedOpenApis,
+                                                           ObjectFactory<OpenAPIService> defaultOpenAPIBuilder, AbstractRequestService requestBuilder,
+                                                           GenericResponseService responseBuilder, OperationService operationParser,
+                                                           RequestMappingInfoHandlerMapping requestMappingHandlerMapping,
+                                                           SpringDocConfigProperties springDocConfigProperties,
+                                                           Optional<ActuatorProvider> actuatorProvider) {
+
+        return new MultipleOpenApiWebFluxResource(groupedOpenApis,
+                defaultOpenAPIBuilder, requestBuilder,
+                responseBuilder, operationParser,
+                requestMappingHandlerMapping,
+                springDocConfigProperties,
+                actuatorProvider) {
+
+
+            public Mono<String> openapiJson(ServerHttpRequest serverHttpRequest, String apiDocsUrl, String group) throws JsonProcessingException {
+                List<ServiceInstance> serviceInstances = discoveryClient.getInstances(group);
+                if (!CollectionUtils.isEmpty(serviceInstances)) {
+                    String gatewayUri = serverHttpRequest.getURI().getScheme() + "://" + serverHttpRequest.getURI().getHost();
+                    if (serverHttpRequest.getURI().getPort() != -1) {
+                        gatewayUri += ":" + serverHttpRequest.getURI().getPort();
+                    }
+                    gatewayUri += "/" + group;
+                    String serviceUri = serviceInstances.get(0).getUri().toString();
+                    String url = serviceUri + "/v3/api-docs";
+                    String openapiJson = restTemplate().getForObject(url, String.class);
+                    openapiJson = openapiJson.replace(serviceUri, gatewayUri);
+                    return Mono.just(openapiJson);
+                }
+                return openapiJson(serverHttpRequest, apiDocsUrl, group);
+            }
+        };
+    }
+}
+
