b9bddcd0abd29268323a3ff9024204bba258f473
89360944@qq.com <89360944@qq.com>
2022-04-13 11:35:28 +0800

    test

--- src/main/java/com/example/mmykgateway/MmykGatewayApplication.java
+++ src/main/java/com/example/mmykgateway/MmykGatewayApplication.java
@@ -44,17 +44,17 @@ public class MmykGatewayApplication {
 //        return groups;
 //    }
 
-    @Bean
-    public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
-        return new OpenAPI()
-                .components(new Components().addSecuritySchemes("basicScheme", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic"))
-                        .addParameters("myHeader1", new Parameter().in("header").schema(new StringSchema()).name("myHeader1")).addHeaders("myHeader2", new Header().description("myHeader2 header").schema(new StringSchema())))
-                .info(new Info()
-                        .title("Petstore API")
-                        .version(appVersion)
-                        .description("This is a sample server Petstore server. You can find out more about Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/). For this sample, you can use the api key `special-key` to test the authorization filters.")
-                        .termsOfService("http://swagger.io/terms/")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
-    }
+//    @Bean
+//    public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+//        return new OpenAPI()
+//                .components(new Components().addSecuritySchemes("basicScheme", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic"))
+//                        .addParameters("myHeader1", new Parameter().in("header").schema(new StringSchema()).name("myHeader1")).addHeaders("myHeader2", new Header().description("myHeader2 header").schema(new StringSchema())))
+//                .info(new Info()
+//                        .title("Petstore API")
+//                        .version(appVersion)
+//                        .description("This is a sample server Petstore server. You can find out more about Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/). For this sample, you can use the api key `special-key` to test the authorization filters.")
+//                        .termsOfService("http://swagger.io/terms/")
+//                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+//    }
 
 }
